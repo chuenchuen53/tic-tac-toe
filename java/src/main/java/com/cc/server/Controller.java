f9e9c3ec92786d45665f9cbf2bfae6450844081e
Chuen <chuen.tam@pinnacle-holdings.hk>
2023-02-18 00:21:31 +0800

    init java server

---
+++ java/src/main/java/com/cc/server/Controller.java
@@ -0,0 +1,12 @@
+package com.cc.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String index(){
+        return "Hello World from tic tac toe!";
+    }
+}
